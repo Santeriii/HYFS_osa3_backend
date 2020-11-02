9beab4a831afa41fc58e40e3dfd08a708626cefa
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-11-02 20:51:56 +0200

    Small fixes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(express.json())
 app.use(morgan(':method :url :status :response-time ms - :res[content-type] :body - :req[content-length]'))
+app.use(cors())
 
 let persons = [
     { id: 1, name: 'Arto Hellas', number: '040-123456' },
@@ -71,7 +73,7 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p><br/>${Date()}`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
